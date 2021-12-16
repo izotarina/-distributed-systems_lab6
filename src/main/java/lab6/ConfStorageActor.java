c64de1ae405fb0dd401bc40878a89d9b569d9a9d
izotarina <irunchikt@mail.ru>
2021-12-17 00:50:13 +0300

    Scripted auto-commit on change (2021-12-17 00:50:13) by gitwatch.sh

--- src/main/java/lab6/ConfStorageActor.java
+++ src/main/java/lab6/ConfStorageActor.java
@@ -1,6 +1,31 @@
 package lab6;
 
 import akka.actor.AbstractActor;
+import akka.japi.Pair;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfStorageActor extends AbstractActor {
+    private final Map<Pair<String, Integer>, StoreTestResult> store = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(StoreTestResult.class, m -> {
+                    Pair<String, Integer> key = new Pair<>(m.getUrl(), m.getRequestCount());
+                    store.put(key, m);
+                })
+                .match(GetTestResult.class, req -> {
+                            Pair<String, Integer> key = new Pair<>(req.getUrl(), req.getRequestCount());
+                            StoreTestResult result = null;
+
+                            if (store.containsKey(key)) {
+                                result = store.get(key);
+                            }
+                            sender().tell(result, self());
+                        }
+                ).build();
+    }
 }
