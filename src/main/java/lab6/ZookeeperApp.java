cd922adab9a61721f7520274f95d4c2c8f9a12b7
izotarina <irunchikt@mail.ru>
2021-12-17 16:20:18 +0300

    Scripted auto-commit on change (2021-12-17 16:20:18) by gitwatch.sh

--- src/main/java/lab6/ZookeeperApp.java
+++ src/main/java/lab6/ZookeeperApp.java
@@ -22,15 +22,6 @@ public class ZookeeperApp {
 
         ZookeeperWatcher zookeeperWatcher = new ZookeeperWatcher();
         ZooKeeper zoo = new ZooKeeper("1MB27.0.0.1MB:21MB81MB", 3000, zookeeperWatcher);
-        zoo.create("/servers/s",
-                "data".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
-                CreateMode.EPHEMERAL_SEQUENTIAL
-);
-        List<String> servers = zoo.getChildren("/servers", this);
-        for (String s: servers) {
-            byte[] data = zoo.getData("/servers/" + s, false, null);
-            System.out.println("server " + s + " data=" + new String(data));
-        }
+
     }
 }
--- src/main/java/lab6/ZookeeperWatcher.java
+++ src/main/java/lab6/ZookeeperWatcher.java
@@ -1,11 +1,24 @@
 package lab6;
 
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+
+import java.util.List;
 
 public class ZookeeperWatcher implements Watcher {
     @Override
     public void process(WatchedEvent watchedEvent) {
-
+        zoo.create("/servers/s",
+                "data".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+        );
+        List<String> servers = zoo.getChildren("/servers", this);
+        for (String s: servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
     }
 }
