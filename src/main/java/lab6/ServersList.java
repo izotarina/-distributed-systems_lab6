d1902cc7a68879ed2e6c4c46a112ef1ef23c9c14
izotarina <irunchikt@mail.ru>
2021-12-17 10:25:45 +0300

    Scripted auto-commit on change (2021-12-17 10:25:45) by gitwatch.sh

--- src/main/java/lab6/ServersList.java
+++ src/main/java/lab6/ServersList.java
@@ -1,4 +1,11 @@
 package lab6;
 
+import java.util.ArrayList;
+
 public class ServersList {
+    private final ArrayList<String> servers;
+
+    public ServersList(ArrayList<String> servers) {
+        this.servers = servers;
+    }
 }
