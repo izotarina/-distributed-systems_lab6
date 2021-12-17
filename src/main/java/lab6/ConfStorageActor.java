d6fb9d7f6f42d238f5270a09890151fe9217f27f
izotarina <irunchikt@mail.ru>
2021-12-17 10:23:37 +0300

    Scripted auto-commit on change (2021-12-17 10:23:37) by gitwatch.sh

--- src/main/java/lab6/ConfStorageActor.java
+++ src/main/java/lab6/ConfStorageActor.java
@@ -4,11 +4,12 @@ import akka.actor.AbstractActor;
 import akka.japi.Pair;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ConfStorageActor extends AbstractActor {
-    private final Map<Pair<String, Integer>, StoreTestResult> store = new HashMap<>();
+    private final ArrayList<String> servers = new ArrayList<>();
 
     @Override
     public Receive createReceive() {
