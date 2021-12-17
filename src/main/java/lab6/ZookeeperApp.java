d1c5a5488cd9171c2f8a219bc903eb3a1893b32d
izotarina <irunchikt@mail.ru>
2021-12-17 16:20:27 +0300

    Scripted auto-commit on change (2021-12-17 16:20:27) by gitwatch.sh

--- src/main/java/lab6/ZookeeperApp.java
+++ src/main/java/lab6/ZookeeperApp.java
@@ -5,12 +5,9 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
-import java.util.List;
 
 public class ZookeeperApp {
     public static void main(String[] args) throws IOException {
