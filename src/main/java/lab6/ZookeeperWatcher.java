7b8f420d62d03fb0f03022c16fe7e0b4d2baf87b
izotarina <irunchikt@mail.ru>
2021-12-17 16:15:51 +0300

    Scripted auto-commit on change (2021-12-17 16:15:51) by gitwatch.sh

--- src/main/java/lab6/ZookeeperWatcher.java
+++ src/main/java/lab6/ZookeeperWatcher.java
@@ -1,6 +1,11 @@
 package lab6;
 
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class ZookeeperWatcher implements Watcher {
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
