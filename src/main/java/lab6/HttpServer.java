1ce65e8a952007b1fb5e6d8ca9e29f8c1ec44d4d
izotarina <irunchikt@mail.ru>
2021-12-17 10:42:13 +0300

    Scripted auto-commit on change (2021-12-17 10:42:13) by gitwatch.sh

--- src/main/java/lab6/HttpServer.java
+++ src/main/java/lab6/HttpServer.java
@@ -1,4 +1,32 @@
 package lab6;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import scala.concurrent.Future;
+
+import static akka.http.javadsl.server.Directives.*;
+import static akka.http.javadsl.server.Directives.complete;
+
 public class HttpServer {
+
+    private Route createRoute(ActorRef routerActor) {
+        Route router = route(
+                path("result", () ->
+                        get(() ->
+                                parameter("packageId", (packageId) ->
+                                {
+                                    Future<Object> result = Patterns.ask(routerActor, new GetTestResults(packageId), 5000);
+                                    return completeOKWithFuture(result, Jackson.marshaller());
+                                }))),
+                path("test", () ->
+                        route(
+                                post(() ->
+                                        entity(Jackson.unmarshaller(InputDataTests.class), msg -> {
+                                            routerActor.tell(msg, ActorRef.noSender());
+                                            return complete("Test started!");
+                                        })))));
+        return router;
+    }
 }
