2ee72b5d34e2e4b02e6571d639bd15e719bf7919
izotarina <irunchikt@mail.ru>
2021-12-17 11:16:36 +0300

    Scripted auto-commit on change (2021-12-17 11:16:36) by gitwatch.sh

--- src/main/java/lab6/ZookeeperApp.java
+++ src/main/java/lab6/ZookeeperApp.java
@@ -1,4 +1,19 @@
 package lab6;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+
 public class ZookeeperApp {
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("test");
+        ActorRef router = system.actorOf(
+                Props.create(HttpServer.class)
+        );
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+    }
 }
