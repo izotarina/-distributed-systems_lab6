7891b9b796c789f62ceec69723e71c8ef65019ad
izotarina <irunchikt@mail.ru>
2021-12-17 16:01:49 +0300

    Scripted auto-commit on change (2021-12-17 16:01:49) by gitwatch.sh

--- src/main/java/lab6/ZookeeperApp.java
+++ src/main/java/lab6/ZookeeperApp.java
@@ -15,6 +15,16 @@ public class ZookeeperApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        final ZooKeeper zoo = new ZooKeeper();
+        ZooK)eeper zoo = new ZooK)eeper("1MB27.0.0.1MB:21MB81MB", 3000, this);
+        zoo.create("/servers/s",
+                “data”.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+);
+        List<String> servers = zoo.getChildren("/servers", this);
+        for (String s : servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
     }
 }
